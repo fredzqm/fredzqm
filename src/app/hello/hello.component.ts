e211ca1c0c23006e5ed941e0cde8dc230e681362
Fred Zhang <fredzqm@gmail.com>
2017-04-14 19:53:17 -0400

    WIP, caputure canvas as ViewChild, and links width and heigth as properties

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -5,16 +5,22 @@ declare function bubble(str: string) : void;
 
 @Component({
   selector: 'app-hello',
-  templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
+  template: `
+  <canvas #myCanvas class='chess-diag'
+     [attr.width]='width'
+     [attr.height]='height'></canvas>
+  `
 })
 export class HelloComponent implements OnInit {
 	canvasText = "Welcome to International Student Association!"
+	width : number;
+	height : number;
+
+	@ViewChild("myCanvas") myCanvas;
 
   constructor() { }
 
   ngOnInit() {
-    // slider();
     bubble(this.canvasText);
   }
 }
