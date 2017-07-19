ede0978ece82b54c3e0fe882aa3f637492132ce3
Fred Zhang <qinzhang@groupon.com>
2017-07-18 17:08:20 -0700

    styles

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -1,7 +1,7 @@
 import {Project} from './project';
-import {chessProject} from "./projects/chess-project";
-import {urlConnectProejct} from "./projects/url-connect-project";
-import {schemeProject} from "./projects/scheme-project/scheme-project";
+import {chessProject} from './projects/chess-project';
+import {urlConnectProejct} from './projects/url-connect-project';
+import {schemeProject} from './projects/scheme-project/scheme-project';
 import {umlGeneratorProject} from './projects/uml-generator-project';
 import {jobeeProject} from './projects/jobee-project';
 import {laplaceProejct} from './projects/laplace-project/laplace-project';
@@ -11,12 +11,12 @@ import {mapProejct} from './projects/map-project';
 
 export const ourProjects: Project[] = [
   chessProject,
-  urlConnectProejct,
-  schemeProject,
   umlGeneratorProject,
-  mapProejct,
+  schemeProject,
   jobeeProject,
+  mapProejct,
+  urlConnectProejct,
+  // operatingSystemProject,
   laplaceProejct,
   diggerGameProject,
-  // operatingSystemProject,
 ];
