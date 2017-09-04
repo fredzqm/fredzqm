b13c6465cb7b5252e68360b74c015a404a797247
Qinmao Zhang <zhangq2@rose-hulman.edu>
2017-09-03 20:30:02 -0400

    update descript

--- src/app/projects/cpu-project/cpu-project.ts
+++ src/app/projects/cpu-project/cpu-project.ts
@@ -3,7 +3,7 @@ import {CPUProjectComponent} from './cpu-project.component';
 
 export const cpuProejct: Project = {
   name: 'CPU Project',
-  information: `This is a mathematical research project about computing inverse laplace transform through numerical method. Based on Post’s Inversion Formula, we are trying to approximate inverse Laplace transform by solving high order differential equations. I built a MATLAB model that uses Picard Iteration to solve high order differential equations that consists of only plus, multiple operation. It can be shown that every differential equation can be converted into such form.`,
+  information: `This is a computer architecture project, where a team of four designed and implemented a 15-bit processor`,
   overview: null,
   image: 'assets/projects/datapath.png',
   repos: 'CPU',
