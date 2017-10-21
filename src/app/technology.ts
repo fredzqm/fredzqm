72df7597930489cf489ebd556802ca763078b48d
Fred Zhang <fredzqm@gmail.com>
2017-10-20 23:54:08 -0400

    added mysql and reorderd technologies based on familiarity

--- src/app/technology.ts
+++ src/app/technology.ts
@@ -77,18 +77,6 @@ export const vendorImages : Technology[] = [{
   imageSrc: 'assets/technologies/ruby.png',
   name: 'Ruby'
 }, {
-  href: 'https://developer.apple.com/swift/',
-  imageSrc: 'assets/technologies/swift.png',
-  name: 'Swift'
-}, {
-  href: 'https://www.android.com/',
-  imageSrc: 'assets/technologies/android.png',
-  name: 'Android'
-}, {
-  href: 'https://developer.apple.com/ios/',
-  imageSrc: 'assets/technologies/ios.jpg',
-  name: 'IOS'
-}, {
   href: 'http://github.com/',
   imageSrc: 'assets/technologies/github.png',
   name: 'Github'
@@ -100,6 +88,46 @@ export const vendorImages : Technology[] = [{
   href: 'https://www.docker.com/',
   imageSrc: 'assets/technologies/docker.svg',
   name: 'Docker'
+}, {
+  href: 'https://www.mysql.com/',
+  imageSrc: 'assets/technologies/MySQL.png',
+  name: 'MySQL'
+}, {
+  href: 'https://www.mongodb.org/',
+  imageSrc: 'assets/technologies/mongodb.png',
+  name: 'Mongodb'
+}, {
+  href: 'https://redis.io/',
+  imageSrc: 'assets/technologies/redis.svg',
+  name: 'Redis'
+}, {
+  href: 'https://gradle.org/',
+  imageSrc: 'assets/technologies/gradle.png',
+  name: 'Gradle'
+}, {
+  href: 'http://maven.apache.org/',
+  imageSrc: 'assets/technologies/maven.png',
+  name: 'Maven'
+}, {
+  href: 'https://projects.spring.io/spring-boot/',
+  imageSrc: 'assets/technologies/spring-boot.png',
+  name: 'Spring Boot'
+}, {
+  href: 'https://nodejs.org/',
+  imageSrc: 'assets/technologies/nodejs.svg',
+  name: 'Node.js'
+}, {
+  href: 'https://yarnpkg.com/en/',
+  imageSrc: 'assets/technologies/yarn.png',
+  name: 'Yarn'
+}, {
+  href: 'https://www.npmjs.com/',
+  imageSrc: 'assets/technologies/npm.svg',
+  name: 'npm'
+}, {
+  href: 'https://webpack.github.io',
+  imageSrc: 'assets/technologies/webpack.png',
+  name: 'Webpack'
 }, {
   href: 'http://hadoop.apache.org/',
   imageSrc: 'assets/technologies/hadoop.png',
@@ -121,10 +149,6 @@ export const vendorImages : Technology[] = [{
   imageSrc: 'assets/technologies/spark.png',
   name: 'Spark'
 }, {
-  href: 'http://oozie.apache.org/',
-  imageSrc: 'assets/technologies/oozie.png',
-  name: 'Oozie'
-}, {
   href: 'http://sqoop.apache.org/',
   imageSrc: 'assets/technologies/sqoop.png',
   name: 'Sqoop'
@@ -137,33 +161,21 @@ export const vendorImages : Technology[] = [{
   imageSrc: 'assets/technologies/kafka.png',
   name: 'kafka'
 }, {
-  href: 'http://maven.apache.org/',
-  imageSrc: 'assets/technologies/maven.png',
-  name: 'Maven'
+  href: 'http://oozie.apache.org/',
+  imageSrc: 'assets/technologies/oozie.png',
+  name: 'Oozie'
 }, {
-  href: 'https://gradle.org/',
-  imageSrc: 'assets/technologies/gradle.png',
-  name: 'Gradle'
+  href: 'https://developer.apple.com/swift/',
+  imageSrc: 'assets/technologies/swift.png',
+  name: 'Swift'
 }, {
-  href: 'https://projects.spring.io/spring-boot/',
-  imageSrc: 'assets/technologies/spring-boot.png',
-  name: 'Spring Boot'
+  href: 'https://www.android.com/',
+  imageSrc: 'assets/technologies/android.png',
+  name: 'Android'
 }, {
-  href: 'https://nodejs.org/',
-  imageSrc: 'assets/technologies/nodejs.svg',
-  name: 'Node.js'
-}, {
-  href: 'https://www.npmjs.com/',
-  imageSrc: 'assets/technologies/npm.svg',
-  name: 'npm'
-}, {
-  href: 'https://yarnpkg.com/en/',
-  imageSrc: 'assets/technologies/yarn.png',
-  name: 'Yarn'
-}, {
-  href: 'https://webpack.github.io',
-  imageSrc: 'assets/technologies/webpack.png',
-  name: 'Webpack'
+  href: 'https://developer.apple.com/ios/',
+  imageSrc: 'assets/technologies/ios.jpg',
+  name: 'IOS'
 }, {
   href: 'http://getbootstrap.com/',
   imageSrc: 'assets/technologies/boostrap.png',
@@ -188,14 +200,6 @@ export const vendorImages : Technology[] = [{
   href: 'http://sass-lang.com/',
   imageSrc: 'assets/technologies/sass.svg',
   name: 'Sass'
-}, {
-  href: 'https://www.mongodb.org/',
-  imageSrc: 'assets/technologies/mongodb.svg',
-  name: 'Mongodb'
-}, {
-  href: 'https://redis.io/',
-  imageSrc: 'assets/technologies/redis.svg',
-  name: 'Redis'
 }, {
   href: 'http://expressjs.com/',
   imageSrc: 'assets/technologies/express.png',
